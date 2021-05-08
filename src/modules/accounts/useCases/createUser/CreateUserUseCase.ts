0b10a360ed2f70e1fbcfd7c60dc18ea475c8f148
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-05-07 22:33:00 -0300

    feat: add validation unique email

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,4 @@
-import { ha } from "bcrypt";
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -17,10 +17,17 @@ class CreateUserUseCase {
         password,
         driver_license,
     }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
         await this.usersRepository.create({
             name,
             email,
-            password,
+            password: passwordHash,
             driver_license,
         });
     }
