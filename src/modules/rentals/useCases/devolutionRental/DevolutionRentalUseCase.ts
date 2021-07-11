873eb509b77b08577f89ee1f8aa4416b2e851335
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-07-11 20:14:45 -0300

    refactor: devolutionRental

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,6 +1,7 @@
 import { inject } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
@@ -22,7 +23,7 @@ class DevolutionRentalUseCase {
         private dateProvider: IDateProvider
     ) {}
 
-    async execute({ id, user_id }: IRequest) {
+    async execute({ id, user_id }: IRequest): Promise<Rental> {
         const rental = await this.rentalsRepository.findById(id);
         const car = await this.carsRepository.findById(id);
         const minimum_daily = 1;
